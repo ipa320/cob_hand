1a748b78b47b16b2f2af252599a648b3697d3334
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2016-07-26 12:41:52 +0200

    implemented recover service

--- cob_hand_bridge/client/src/bridge.cpp
+++ cob_hand_bridge/client/src/bridge.cpp
@@ -9,30 +9,62 @@
 #include <std_srvs/Trigger.h>
 
 #include <boost/thread/thread.hpp>
+#include <boost/scoped_ptr.hpp>
 
 #include "hardware.h"
 #include "gpio.h"
 #include "sdhx.h"
 
 ros::NodeHandle_<HandBridgeHardware> g_nh;
+cob_hand_bridge::Status g_status_msg;
+
 
 // SDHX
-SDHX g_sdhx;
+boost::scoped_ptr<SDHX> g_sdhx;
 
 using cob_hand_bridge::InitFinger;
+
+InitFinger::Request g_init_req;
+
+bool initFinger(const InitFinger::Request & req){
+    if(!g_sdhx){
+        g_sdhx.reset(new SDHX);
+        if(g_sdhx->init(req.port, req.min_pwm0, req.min_pwm1, req.max_pwm0, req.max_pwm1)){
+            g_init_req = req;
+            return true;
+        }
+    }
+    return false;
+}
+
 void handleInitFinger(const InitFinger::Request & req, InitFinger::Response & res){
-    res.success = g_sdhx.init(req.port, req.min_pwm0, req.min_pwm1, req.max_pwm0, req.max_pwm1);
+    res.success = initFinger(req);
 }
 ros::ServiceServer<InitFinger::Request, InitFinger::Response> g_srv_init_finger("init_finger",&handleInitFinger);
 
 using std_srvs::Trigger;
 void handleHaltFinger(const Trigger::Request & req, Trigger::Response & res){
-    res.success = g_sdhx.halt();
+    res.success = g_sdhx && g_sdhx->halt();
 }
 ros::ServiceServer<Trigger::Request, Trigger::Response> g_srv_halt_finger("halt",&handleHaltFinger);
 
+void handleRecover(const Trigger::Request & req, Trigger::Response & res){
+    if(g_sdhx) {
+        if((g_status_msg.status & g_status_msg.MASK_ERROR) == 0) {
+            res.success = true;
+        }else if(g_sdhx->isInitialized()) {
+            g_sdhx.reset();
+            res.success = initFinger(g_init_req);
+            if(res.success) g_status_msg.status &= ~g_status_msg.MASK_ERROR;
+        }else{
+            res.message = "Not initialized";
+        }
+    }
+}
+ros::ServiceServer<Trigger::Request, Trigger::Response> g_srv_recover("recover",&handleRecover);
+
 void handleJointCommand(const cob_hand_bridge::JointValues& j){
-    g_sdhx.move(j.position_cdeg, j.velocity_cgeg, j.current_100uA);
+    if(g_sdhx) g_sdhx->move(j.position_cdeg, j.velocity_cgeg, j.current_100uA);
 }
 ros::Subscriber<cob_hand_bridge::JointValues> g_sub_command("command", handleJointCommand );
 
@@ -72,7 +104,6 @@ void handleSetPin(const std_msgs::UInt8& mgs){
 }
 ros::Subscriber<std_msgs::UInt8> g_sub_set_pin("set_pin", handleSetPin );
 
-cob_hand_bridge::Status g_status_msg;
 ros::Publisher g_pub("status", &g_status_msg);
 
 using cob_hand_bridge::SetPWM;
@@ -99,10 +130,10 @@ void step(){
 
     cob_hand_bridge::JointValues &j= g_status_msg.joints;
 
-    if(g_sdhx.isInitialized()){
-        if(g_sdhx.getData(j.position_cdeg, j.velocity_cgeg, j.current_100uA, boost::chrono::seconds(1))) g_status_msg.status |= g_status_msg.MASK_FINGER_READY;
+    if(g_sdhx && g_sdhx->isInitialized()){
+        if(g_sdhx->getData(j.position_cdeg, j.velocity_cgeg, j.current_100uA, boost::chrono::seconds(1))) g_status_msg.status |= g_status_msg.MASK_FINGER_READY;
         else g_status_msg.status |= g_status_msg.MASK_ERROR;
-        g_status_msg.rc = g_sdhx.getRC();
+        g_status_msg.rc = g_sdhx->getRC();
     }
 
     g_status_msg.pins = g_gpio.getState();
@@ -110,7 +141,7 @@ void step(){
     g_pub.publish( &g_status_msg );
     g_nh.spinOnce();
 
-    g_sdhx.poll();
+    if(g_sdhx) g_sdhx->poll();
 
     ++g_status_msg.seq;
 }
