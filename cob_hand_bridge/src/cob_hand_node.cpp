d8c090985b86cd4710e2dc8978f4423fcc88ab34
ipa-fmw <florian.weisshardt@unity-robotics.de>
2018-03-16 09:21:55 +0100

    mojin robotics copyright

--- cob_hand_bridge/src/cob_hand_node.cpp
+++ cob_hand_bridge/src/cob_hand_node.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Fraunhofer Institute for Manufacturing Engineering and Automation (IPA)
+ * Copyright 2017 Mojin Robotics
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
